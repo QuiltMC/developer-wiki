578377cc9b6436768dd8d0c97143b9e59615688c
tibs <git@tibinonest.me>
2023-06-15 15:50:00 -0400

    Cleanup

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
-import { mdsvex } from 'mdsvex'
+import { mdsvex } from 'mdsvex';
 
 export default {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	},
 
 	extensions: ['.svelte', '.md'],
