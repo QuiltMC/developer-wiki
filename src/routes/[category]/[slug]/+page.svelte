7780a648b52371b1198df91a69a32d40d9eb70bf
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2023-09-03 22:38:27 +0200

    Add a Table of contents

--- src/routes/[category]/[slug]/+page.svelte
+++ src/routes/[category]/[slug]/+page.svelte
@@ -6,6 +6,8 @@
 	<title>{data.title}</title>
 </svelte:head>
 
-<main class="content column">
-	<svelte:component this={data.content} />
+<main class="column ">
+	<div class="columns" style="flex-direction: row-reverse;">
+		<svelte:component this={data.content} />
+	</div>
 </main>
--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import { vitePreprocess } from "@sveltejs/kit/vite";
 import { mdsvex } from "mdsvex";
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
+import toc from '@jsdevtools/rehype-toc';
+import sectionize from '@hbsnow/rehype-sectionize';
+import { children } from "svelte/internal";
 
 export default {
 	kit: {
@@ -22,7 +25,41 @@ export default {
 			extensions: [".md"],
 			rehypePlugins: [
 				rehypeSlug,
-				rehypeAutolinkHeadings
+				rehypeAutolinkHeadings,
+				[sectionize, {properties: {className: "column content"}}],
+				[toc, {
+					nav: false,
+					customizeTOC: (toc) =>{
+						toc.properties.className = "menu-list";
+						function change_tagname(node) {
+							if (node.tagName == "ol") node.tagName = "ul";
+							if (node.children) node.children.forEach(change_tagname);
+						}
+						change_tagname(toc);
+						return {
+							type: 'element',
+							tagName: 'div',
+							properties: {className: "column is-narrow"},
+							children: [{
+								type: 'element',
+								tagName: 'div',
+								properties: {className: "box is-hidden-mobile is-sticky sidebar"},
+								children: [{
+									type: 'element',
+									tagName: 'aside',
+									properties: {className: "menu"},
+									children: [{
+										type: 'element',
+										tagName: 'p',
+										properties: {className: 'menu-label'},
+										children: [{type: 'text', value: 'Contents'}]
+									},
+									toc
+								]}]
+							}]
+						};
+					}
+				}]
 			]
 		})
 	]
