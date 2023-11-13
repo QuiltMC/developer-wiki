6e9a70ed965f8798fdccd2def81e3c6361565855
celeri <celestin.bouchet@gmail.com>
2023-11-13 13:39:09 -0500

    Improved the usability of the lang dropdown

--- src/lib/NavbarLangDropdown.svelte
+++ src/lib/NavbarLangDropdown.svelte
@@ -1,24 +1,38 @@
 <script lang="ts">
-	import { browser } from "$app/environment";
-	import { locale, locales, t } from "$lib/translations";
-	import { onDestroy } from "svelte";
-	import type { Unsubscriber } from "svelte/store";
+	import { browser } from '$app/environment';
+	import { locale, locales, t } from '$lib/translations';
+	import { onDestroy } from 'svelte';
+	import type { Unsubscriber } from 'svelte/store';
 
 	let unsubscriber: Unsubscriber;
 	onDestroy(() => unsubscriber && unsubscriber());
 
 	if (browser) {
-		const user_locale: string = window.localStorage.getItem("lang") || window.navigator.language;
+		const user_locale: string = window.localStorage.getItem('lang') || window.navigator.language;
 		if ($locales.includes(user_locale)) {
 			$locale = user_locale;
 		}
 
-		unsubscriber = locale.subscribe((locale) => localStorage.setItem("lang", locale));
+		unsubscriber = locale.subscribe((locale) => localStorage.setItem('lang', locale));
 	}
 
 	let is_dropdown_active = false;
 	function toggleNavbar() {
 		is_dropdown_active = !is_dropdown_active;
+	}
+
+	let dropdown_toggle: HTMLDivElement;
+	function handleBlur(event: FocusEvent) {
+		// Check that the newly focused element is not a child
+		// of the dropdown toggle element so that users
+		// can actually select a locale
+		if (
+			!event.relatedTarget ||
+			!(event.relatedTarget instanceof Node) ||
+			!dropdown_toggle.contains(event.relatedTarget)
+		) {
+			is_dropdown_active = false;
+		}
 	}
 
 	const selectLocale = (selected_locale: string) => {
@@ -29,25 +43,19 @@
 <div
 	class="navbar-item has-dropdown {is_dropdown_active ? 'is-active' : ''}"
 	on:click={toggleNavbar}
-	on:keypress={toggleNavbar}
-	on:blur={(event) => {
-		// Check that the newly focused element is not a child
-		// of this element so that users can actually select
-		// a locale
-		if (
-			!event.relatedTarget ||
-			!(event.relatedTarget instanceof Node) ||
-			!event.currentTarget.contains(event.relatedTarget)
-		) {
-			is_dropdown_active = false;
+	on:keypress={(event) => {
+		if (event.key === 'Enter') {
+			toggleNavbar();
 		}
 	}}
+	on:blur={handleBlur}
+	bind:this={dropdown_toggle}
 	role="button"
 	tabindex="0"
 >
 	<span class="navbar-link has-icon">
 		<span class="icon"><i class="fas fa-language fa-xl" /></span>
-		<span class="is-hidden-desktop">{$t("application.lang-dropdown.language")}</span>
+		<span class="is-hidden-desktop">{$t('application.lang-dropdown.language')}</span>
 	</span>
 
 	<div class="navbar-dropdown">
@@ -55,7 +63,12 @@
 			<span
 				class="navbar-item is-clickable"
 				on:click={() => selectLocale(locale)}
-				on:keypress={() => selectLocale(locale)}
+				on:keypress={(event) => {
+					if (event.key === 'Enter') {
+						selectLocale(locale);
+					}
+				}}
+				on:blur={handleBlur}
 				role="button"
 				tabindex="0"
 			>
