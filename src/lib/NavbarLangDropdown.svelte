eb940a1f23963a5cab8b8875c38f4a2ef1dd906e
celeri <celestin.bouchet@gmail.com>
2023-11-13 02:43:32 -0500

    Changed the behavior of the lang dropdown so that it toggles on click instead of opening when hovered

--- src/lib/NavbarLangDropdown.svelte
+++ src/lib/NavbarLangDropdown.svelte
@@ -4,9 +4,8 @@
 	import { onDestroy } from "svelte";
 	import type { Unsubscriber } from "svelte/store";
 
-	const on_lang_item_action = (selected_locale: string) => ($locale = selected_locale);
-
 	let unsubscriber: Unsubscriber;
+	onDestroy(() => unsubscriber && unsubscriber());
 
 	if (browser) {
 		const user_locale: string = window.localStorage.getItem("lang") || window.navigator.language;
@@ -14,28 +13,53 @@
 			$locale = user_locale;
 		}
 
-		locale.subscribe((locale) => localStorage.setItem("lang", locale));
+		unsubscriber = locale.subscribe((locale) => localStorage.setItem("lang", locale));
 	}
 
-	onDestroy(() => unsubscriber && unsubscriber());
+	let is_dropdown_active = false;
+	function toggleNavbar() {
+		is_dropdown_active = !is_dropdown_active;
+	}
+
+	const selectLocale = (selected_locale: string) => {
+		$locale = selected_locale;
+	};
 </script>
 
-<div class="navbar-item has-dropdown is-hoverable">
+<div
+	class="navbar-item has-dropdown {is_dropdown_active ? 'is-active' : ''}"
+	on:click={toggleNavbar}
+	on:keypress={toggleNavbar}
+	on:blur={(event) => {
+		// Check that the newly focused element is not a child
+		// of this element so that users can actually select
+		// a locale
+		if (
+			!event.relatedTarget ||
+			!(event.relatedTarget instanceof Node) ||
+			!event.currentTarget.contains(event.relatedTarget)
+		) {
+			is_dropdown_active = false;
+		}
+	}}
+	role="button"
+	tabindex="0"
+>
 	<span class="navbar-link has-icon">
 		<span class="icon"><i class="fas fa-language fa-xl" /></span>
 		<span class="is-hidden-desktop">{$t("application.lang-dropdown.language")}</span>
 	</span>
 
 	<div class="navbar-dropdown">
-		{#each $locales as value}
+		{#each $locales as locale}
 			<span
 				class="navbar-item is-clickable"
-				on:click={() => on_lang_item_action(value)}
-				on:keypress={() => on_lang_item_action(value)}
+				on:click={() => selectLocale(locale)}
+				on:keypress={() => selectLocale(locale)}
 				role="button"
 				tabindex="0"
 			>
-				{$t(`lang.${value}`)}
+				{$t(`lang.${locale}`)}
 			</span>
 		{/each}
 	</div>
