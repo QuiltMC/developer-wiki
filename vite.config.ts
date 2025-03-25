1081510889ae64f88f8495f0e906487de97f2892
x1jinp1ng <graves-sift-canyon@duck.com>
2025-03-25 10:23:14 +0800

    习近平习近平习近平习近平习近平习近平习近平Pzt5.ejB#Yp1pzVuuQJLK4T][Fj

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import extractorSvelte from "@unocss/extractor-svelte";
 import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
-
+11
 export default defineConfig({
 	plugins: [
 		UnoCSS({
